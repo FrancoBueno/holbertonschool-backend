db9e5d006e06cda384bf3795f61934424b62854a
root <3290@holbertonschool.com>
2022-05-11 18:53:33 +0000

    aver

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,7 +10,7 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         if item is None or key is None:
-            pass
+            return
         else:
             self.cache_data[key] = item
 
